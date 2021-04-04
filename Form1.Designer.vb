e1bd96e793f27bcff07d2fb2dc9b1d56944ef844
Ray Held <40300226+RayGHeld@users.noreply.github.com>
2021-04-04 14:38:40 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSignIn = New System.Windows.Forms.Button()
+        Me.txtBoxAccessToken = New System.Windows.Forms.TextBox()
+        Me.txtboxIDToken = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnClientCredentials = New System.Windows.Forms.Button()
+        Me.txtBoxScopes = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSignIn
+        '
+        Me.btnSignIn.Location = New System.Drawing.Point(12, 12)
+        Me.btnSignIn.Name = "btnSignIn"
+        Me.btnSignIn.Size = New System.Drawing.Size(148, 23)
+        Me.btnSignIn.TabIndex = 0
+        Me.btnSignIn.Text = "Sign In Interactive"
+        Me.btnSignIn.UseVisualStyleBackColor = True
+        '
+        'txtBoxAccessToken
+        '
+        Me.txtBoxAccessToken.Location = New System.Drawing.Point(12, 253)
+        Me.txtBoxAccessToken.Multiline = True
+        Me.txtBoxAccessToken.Name = "txtBoxAccessToken"
+        Me.txtBoxAccessToken.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtBoxAccessToken.Size = New System.Drawing.Size(776, 185)
+        Me.txtBoxAccessToken.TabIndex = 1
+        '
+        'txtboxIDToken
+        '
+        Me.txtboxIDToken.Location = New System.Drawing.Point(12, 75)
+        Me.txtboxIDToken.Multiline = True
+        Me.txtboxIDToken.Name = "txtboxIDToken"
+        Me.txtboxIDToken.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtboxIDToken.Size = New System.Drawing.Size(776, 142)
+        Me.txtboxIDToken.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Id Token:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 234)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Access Token"
+        '
+        'btnClientCredentials
+        '
+        Me.btnClientCredentials.Location = New System.Drawing.Point(507, 44)
+        Me.btnClientCredentials.Name = "btnClientCredentials"
+        Me.btnClientCredentials.Size = New System.Drawing.Size(148, 23)
+        Me.btnClientCredentials.TabIndex = 5
+        Me.btnClientCredentials.Text = "Sign in Client Credentials"
+        Me.btnClientCredentials.UseVisualStyleBackColor = True
+        '
+        'txtBoxScopes
+        '
+        Me.txtBoxScopes.Location = New System.Drawing.Point(507, 18)
+        Me.txtBoxScopes.Name = "txtBoxScopes"
+        Me.txtBoxScopes.Size = New System.Drawing.Size(281, 20)
+        Me.txtBoxScopes.TabIndex = 6
+        Me.txtBoxScopes.Text = "https://graph.microsoft.com/.default"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(458, 21)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Scopes"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtBoxScopes)
+        Me.Controls.Add(Me.btnClientCredentials)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtboxIDToken)
+        Me.Controls.Add(Me.txtBoxAccessToken)
+        Me.Controls.Add(Me.btnSignIn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSignIn As Button
+    Friend WithEvents txtBoxAccessToken As TextBox
+    Friend WithEvents txtboxIDToken As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnClientCredentials As Button
+    Friend WithEvents txtBoxScopes As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,159 @@
+﻿Imports System.Text
+Imports Microsoft.Identity.Client
+
+Public Class Form1
+
+    Private accessToken As String = String.Empty
+    Private idToken As String = String.Empty
+
+    Private client_id As String = ""
+    Private client_secret As String = ""
+    Private tenant_id As String = ""
+    Private redirect_uri As String = "http://localhost"
+    Private scopes() As String = New String() {"openid offline_access profile "}
+
+    Private errors As New Stack(Of String)
+
+    Private isLoggedIn As Boolean = False
+
+    Private Sub btnSignIn_Click(sender As Object, e As EventArgs) Handles btnSignIn.Click
+
+        txtboxIDToken.Text = String.Empty
+        txtBoxAccessToken.Text = String.Empty
+
+        idToken = String.Empty
+        accessToken = String.Empty
+
+        'we need a task to get MSAL to log us in
+        If (txtBoxScopes.Text.Length > 0) Then
+            Try
+                Dim _scopes() As String = txtBoxScopes.Text.Split(" ")
+                scopes = _scopes
+            Catch ex As Exception
+                MessageBox.Show("Invalid scopes parameter... resetting to openid offline_access profile")
+                txtBoxScopes.Text = "openid offline_access profile"
+                txtBoxScopes.Focus()
+                txtBoxScopes.SelectAll()
+                Exit Sub
+            End Try
+        End If
+
+        Dim task = New Task(AddressOf LoginInteractively)
+        task.Start()
+        task.Wait()
+
+        If errors.Count > 0 Then
+            Dim error_messages As New StringBuilder()
+            Do Until errors.Count <= 0
+                If error_messages.Length > 0 Then
+                    error_messages.Append(ControlChars.NewLine)
+                End If
+                error_messages.Append(errors.Pop())
+            Loop
+            MessageBox.Show($"Errors encountered: {error_messages.ToString()}")
+        End If
+
+    End Sub
+
+    Private Sub btnClientCredentials_Click(sender As Object, e As EventArgs) Handles btnClientCredentials.Click
+        txtboxIDToken.Text = String.Empty
+        txtBoxAccessToken.Text = String.Empty
+
+        idToken = String.Empty
+        accessToken = String.Empty
+
+        'we need a task to get MSAL to log us in
+        If (txtBoxScopes.Text.Length > 0) Then
+            Try
+                Dim _scopes() As String = txtBoxScopes.Text.Split(" ")
+                scopes = _scopes
+            Catch ex As Exception
+                MessageBox.Show("Invalid scopes parameter... resetting to https://graph.microsoft.com/.default")
+                txtBoxScopes.Text = "https://graph.microsoft.com/.default"
+                txtBoxScopes.Focus()
+                txtBoxScopes.SelectAll()
+                Exit Sub
+            End Try
+        End If
+
+        Dim task = New Task(AddressOf LoginClientCredentials)
+        task.Start()
+        task.Wait()
+
+        If errors.Count > 0 Then
+            Dim error_messages As New StringBuilder()
+            Do Until errors.Count <= 0
+                If error_messages.Length > 0 Then
+                    error_messages.Append(ControlChars.NewLine)
+                End If
+                error_messages.Append(errors.Pop())
+            Loop
+            MessageBox.Show($"Errors encountered: {error_messages.ToString()}")
+        End If
+
+    End Sub
+
+    Private Async Sub LoginInteractively()
+        Try
+            Dim app As IPublicClientApplication = PublicClientApplicationBuilder.Create(client_id).WithRedirectUri(redirect_uri).WithTenantId(tenant_id).WithAuthority(AadAuthorityAudience.AzureAdMyOrg).Build()
+            Dim authResult As AuthenticationResult = Nothing
+
+            Dim accounts As IEnumerable(Of IAccount) = Await app.GetAccountsAsync()
+            Dim performInterativeFlow As Boolean = False
+
+            Try
+                authResult = Await app.AcquireTokenSilent(scopes, accounts.FirstOrDefault()).ExecuteAsync()
+            Catch ex As MsalUiRequiredException
+                performInterativeFlow = True
+            Catch ex As Exception
+                errors.Push(ex.Message)
+            End Try
+
+            If performInterativeFlow Then
+                authResult = Await app.AcquireTokenInteractive(scopes).ExecuteAsync()
+            End If
+
+            If authResult.AccessToken <> String.Empty Then
+                accessToken = authResult.AccessToken
+                idToken = authResult.IdToken
+            End If
+
+        Catch ex As Exception
+            errors.Push(ex.Message)
+            Exit Sub
+        End Try
+
+        'Since this thread runs under the ui thread, we need a delegate method to update the text boxes
+        txtBoxAccessToken.BeginInvoke(New InvokeDelegate(AddressOf InvokeMethod))
+
+        Return
+    End Sub
+
+    Private Async Sub LoginClientCredentials()
+
+        Dim authResult As AuthenticationResult = Nothing
+
+        Try
+            Dim app As IConfidentialClientApplication = ConfidentialClientApplicationBuilder.Create(client_id).WithClientSecret(client_secret).WithTenantId(tenant_id).WithAuthority(AadAuthorityAudience.AzureAdMyOrg).Build()
+            authResult = Await app.AcquireTokenForClient(scopes).ExecuteAsync()
+        Catch ex As Exception
+            errors.Push(ex.Message)
+            Exit Sub
+        End Try
+
+        accessToken = authResult.AccessToken
+        idToken = "No id token given for this auth flow."
+
+        'Since this thread runs under the ui thread, we need a delegate method to update the text boxes
+        txtBoxAccessToken.BeginInvoke(New InvokeDelegate(AddressOf InvokeMethod))
+    End Sub
+
+    Private Delegate Sub InvokeDelegate()
+    Private Sub InvokeMethod()
+        txtBoxAccessToken.Text = accessToken
+        txtboxIDToken.Text = idToken
+    End Sub
+
+
+End Class
+
